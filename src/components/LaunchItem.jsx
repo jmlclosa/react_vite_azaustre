5363b3af68fe835fa10d552389b5b2fe32fa385c
jlopez <juanma.lopezclosa@plexus.es>
2022-04-20 23:28:40 +0200

    Added button (Without event handler)

--- src/components/LaunchItem.jsx
+++ src/components/LaunchItem.jsx
@@ -1,5 +1,7 @@
 import { Card } from 'primereact/card';
 import { Tag } from 'primereact/tag';
+import { Divider } from 'primereact/divider';
+import { Button } from 'primereact/button';
 import * as dayjs from 'dayjs'
 
 export function LaunchItem(launch) {
@@ -8,6 +10,8 @@ export function LaunchItem(launch) {
             <Tag className="absolute top-50 right-0" value={launch.launch_success ? "Success" : "Failure"} severity={launch.launch_success ? "success" : "danger"} />
             <i className="pi pi-calendar"></i>
             {dayjs(launch.launch_date_local).locale("es").format("D MMMM, YYYY")}
+            <Divider />
+            <Button label="More details" className="p-button-info" icon="pi pi-check" iconPos="right" />
         </Card>
     )
 }
