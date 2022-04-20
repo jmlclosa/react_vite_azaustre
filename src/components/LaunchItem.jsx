a2417169b661187dcb2fcbe3bcf063e820381e8c
jlopez <juanma.lopezclosa@plexus.es>
2022-04-20 23:16:44 +0200

    Fix naming LaunchItem as .jsx file

--- src/components/LaunchItem.jsx
+++ src/components/LaunchItem.jsx
@@ -3,9 +3,9 @@ import { Tag } from 'primereact/tag';
 
 export function LaunchItem(launch) {
     return (
-    <Card title={launch.mission_name} className="relative mb-3">
+    <Card title={launch.mission_name + " at " + launch.launch_year} className="relative mb-3">
         <Tag className="absolute top-50 right-0" value={launch.launch_success ? "Success" : "Failure"} severity={launch.launch_success ? "success" : "danger"} />
-        <i className="pi pi-calendar"></i> {launch.launch_year}
+        <i className="pi pi-calendar"></i>
     </Card>
     )
 }
